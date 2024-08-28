56cb0d4721ff6762d7d5c167ca32238a2e2c95a2
Christian <christian.steen.mortensen@regionh.dk>
2024-08-28 12:35:03 +0200

    Solution for 2015-05 + boilerplate for next few days.

--- python/2015/05/solution.py
+++ python/2015/05/solution.py
@@ -13,54 +13,27 @@ else:
     data = inputdata
 
 def solution(part):
-    if part == 1:
-        nice = 0
-        
-        for string in data:
-            if len(string) - len(string.replace("a", "").replace("e", "").replace("i", "").replace("o", "").replace("u", "")) < 3:
-                continue
+    answer = 0
+    for string in data:
+        req1 = True if sum([x in 'aeiou' for x in string]) >= 3 else False
+        req2 = True if any(x[0] == x[1] for x in [string[i:i+2] for i in range(len(string) - 1)]) else False
+        req3 = True if not any(x in ['ab', 'cd', 'pq', 'xy'] for x in [string[i:i+2] for i in range(len(string) - 1)]) else False
+        pairs = [string[i:i+2] for i in range(len(string) - 1)]
+        req4 = False
+        for i, pair in enumerate(pairs):
+            if pair in pairs[i+2:]:
+                req4 = True
+                break
+        req5 = True if any(x[0] == x[2] for x in [string[i:i+3] for i in range(len(string) - 2)]) else False
 
-            ok = 0
-            for i in range(len(string)):
-                if i != 0:
-                    if string[i - 1] == string[i]:
-                        ok += 1
+        if part == 1:
+            if req1 and req2 and req3:
+                answer += 1
+        if part == 2:
+            if req4 and req5:
+                answer += 1
 
-            if len(string) - len(string.replace("ab", "").replace("cd", "").replace("pq", "").replace("xy", "")) != 0:
-                continue
-
-            if ok > 0:
-                nice += 1
-
-    elif part == 2:
-        nice = 0
-
-        for string in data:
-            print(string)
-            ok = 0
-            for i in range(len(string)):
-                try:
-                    if string[i:i+2] in string[i+2:]:
-                        ok += 1
-                        print("Passed first test: ", string[i:i+2])
-                        break
-                except:
-                    pass
-            for i in range(len(string)):
-                try:
-                    if string[i] == string[i+2] and string[i] != string[i+1]:
-                        ok += 1
-                        print("Passed second test: ", string[i:i+3])
-                        break
-                except:
-                    pass
-
-            if ok >= 2:
-                nice += 1
-            print(f"{nice = }")
-            print()
-
-    return nice
+    return answer
 
     
 print("Part 1:", solution(1))
---
+++ python/2015/06/solution.py
@@ -0,0 +1,47 @@
+import re
+import sys
+from collections import defaultdict
+
+with open("test.txt") as f:
+    testdata = f.read().splitlines()
+
+with open("input.txt") as f:
+    inputdata = f.read().splitlines()
+
+if sys.argv[1] == "test":
+    data = testdata
+else:
+    data = inputdata
+
+def solution(part):
+    answer = 0
+
+    lights = defaultdict(int)
+    for x in range(1000):
+        for y in range(1000):
+            lights[(x, y)] = 0
+
+    for line in data:
+        instructions = re.findall(r"(\w+) (\d+),(\d+)", line)
+        action = instructions[0][0]
+        coords1 = [int(x) for x in [instructions[0][1], instructions[0][2]]]
+        coords2 = [int(x) for x in [instructions[1][1], instructions[1][2]]]
+        for x in range(coords1[0], coords2[0] + 1):
+            for y in range(coords1[1], coords2[1] + 1):
+                if action == "on":
+                    lights[(x, y)] = 1 if part == 1 else lights[(x, y)] + 1
+                elif action == "off":
+                    lights[(x, y)] = 0 if part == 1 else lights[(x, y)] - 1
+                elif action == "toggle":
+                    lights[(x, y)] = 1 - lights[(x, y)] if part == 1 else lights[(x, y)] + 2
+    
+    for k, v in lights.items():
+        if v == 1:
+            answer += 1
+
+    return answer
+    
+print("Part 1:", solution(1))
+print("Part 2:", solution(2))
+
+# 378838 -- too low
