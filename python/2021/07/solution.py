9f89789215cde9db7a65c5a29753f842be5c01c7
Christian <christian.steen.mortensen@regionh.dk>
2024-09-04 11:30:24 +0200

    Solution to 2021-07

--- python/2021/07/solution.py
+++ python/2021/07/solution.py
@@ -1,5 +1,5 @@
 import sys
-from collections import defaultdict
+import math
 
 with open("test.txt") as f:
     testdata = f.read().splitlines()
@@ -13,9 +13,25 @@ else:
     data = inputdata
 
 def solution(part):
-    answer = 1
+    crabs = [int(x) for x in data[0].split(",")]
 
-    return answer
+    min_crab = min(crabs)
+    max_crab = max(crabs)
+
+    lowest = math.inf
+    # Possible positions to align
+    for i in range(min_crab, max_crab + 1):
+        total = 0
+        for crab in crabs:
+            if part == 1:
+                total = total + abs(crab - i)
+            elif part == 2:
+                for j in range(1, abs(crab - i) + 1):
+                    total = total + j
+        if total < lowest:
+            lowest = total
+
+    return lowest
 
 print("Part 1:", solution(1))
 print("Part 2:", solution(2))
