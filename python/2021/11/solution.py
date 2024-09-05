6d7921aa7962b5250fae93d8dc34a77d760aec7a
Christian <christian.steen.mortensen@regionh.dk>
2024-09-05 10:03:37 +0200

    Solution 2021-11.

--- python/2021/11/solution.py
+++ python/2021/11/solution.py
@@ -1,5 +1,5 @@
 import sys
-import math
+from collections import defaultdict
 
 with open("test.txt") as f:
     testdata = f.read().splitlines()
@@ -13,46 +13,57 @@ else:
     data = inputdata
 
 def solution(part):
-    part1 = 0
-    part2 = []
-    pairs = {
-        '(': ')',
-        '[': ']',
-        '{': '}',
-        '<': '>',
-    }
-    points = {
-        ')': [3, 1],
-        ']': [57, 2],
-        '}': [1197, 3],
-        '>': [25137, 4]
-    }
+    grid = defaultdict(int)
+    flashes = 0
 
-    for line in data:
-        expected = []
-        line_corrupted = False
-        for char in line:
-            if char in pairs.keys():
-                expected.append(pairs[char])
-            elif char == expected[-1]:
-                expected.pop()
-            else:
-                part1 += points[char][0]
-                line_corrupted = True
-                break
+    for x in range(len(data[0])):
+        for y in range(len(data)):
+            grid[(x, y)] = int(data[x][y])
 
-        if line_corrupted == False:
-            part2_score = 0
-            for char in expected[::-1]:
-                part2_score *= 5
-                part2_score += points[char][1]
-            part2.append(part2_score)
-        part2.sort()
+    step = 0
+    while True:
+        step += 1
+        # Increase all octopi by 1
+        for k in grid.keys():
+            grid[k] = grid[k] + 1
+        # Set intial flashing octopi
+        flashed = set()
+        for k, v in grid.items():
+            if v >= 10:
+                flashed.add(k)
+        # Make a queue flashing octopi to affect nearby
+        to_handle = []
+        for f in flashed:
+            to_handle.append(f)
+        while to_handle:
+            current = to_handle.pop()
+            adjacents = []
+            for x in range(-1, 2):
+                for y in range(-1, 2):
+                    coords = (current[0] + x, current[1] + y)
+                    if coords in grid.keys():
+                        adjacents.append(coords)
+            for adjacent in adjacents:
+                grid[adjacent] = grid[adjacent] + 1
+            for k, v in grid.items():
+                if v >= 10:
+                    if k in flashed:
+                        pass
+                    else:
+                        flashed.add(k)
+                        to_handle.append(k)
+        # Reset all flashed octopi
+        for k, v in grid.items():
+            if v >= 10:
+                grid[k] = 0
+                flashes += 1
 
-    if part == 1:
-        return part1
-    elif part == 2:
-        return part2[len(part2) // 2]
+        if part == 1:
+            if step == 100:
+                return flashes
+        if part == 2:
+            if set([v for v in grid.values()]) == {0}:
+                return step
 
 print("Part 1:", solution(1))
 print("Part 2:", solution(2))
