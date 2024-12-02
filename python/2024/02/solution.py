713da29276dd6186abd137f637d0eaa11b6a9970
Christian <christian.steen.mortensen@regionh.dk>
2024-12-02 09:37:43 +0100

    Solution 2024 day 02

--- python/2024/02/solution.py
+++ python/2024/02/solution.py
@@ -11,17 +11,43 @@ if sys.argv[1] == "test":
 else:
     data = inputdata
 
+def report_checker(report):
+    safe = 1
+    increasing = 0
+    decreasing = 0
+    for i in range(1, len(report)):
+        if increasing > 0 and decreasing > 0:
+            safe = 0
+            break
+        if report[i] == report[i - 1]:
+            safe = 0
+            break
+        if abs(report[i] - report[i - 1]) > 3:
+            safe = 0
+            break
+        if report[i] > report[i - 1]:
+            increasing += 1
+        if report[i] < report[i - 1]:
+            decreasing += 1
+    return safe
+
 def solution(part):
-    elves = []
-    current = 0
+    count = 0
     for line in data:
-        if line == "":
-            elves.append(current)
-            current = 0
-        else:
-            current += int(line)
-    elves.sort(reverse = True)
-    return sum(elves[0:1 if part == 1 else 3])
+        report = [int(x) for x in line.split()]
+        if part == 1:
+            count += report_checker(report)
+        elif part == 2:
+            subreports = []
+            for i in range(len(report)):
+                subreport = report[:i] + report[i + 1:]
+                subreports.append(subreport)
+                subreports.append(report)
+            for sr in subreports:
+                if report_checker(sr) == 1:
+                    count += 1
+                    break
+    return count
     
 print("Part 1:", solution(1))
 print("Part 2:", solution(2))
